9c9d20860b6470bd0130f7974a5effcf9faf074c
Ayyappa Swamy <153265218+AyyappaSwamy121@users.noreply.github.com>
2025-07-28 23:17:38 +0530

    merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,30 @@
+def merge(arr):
+    if len(arr)>1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid : ]
+        merge(left)
+        merge(right)
+      
+        i,j,k=0
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                arr[k]=left[i]
+                i+=1
+            else:
+                arr[k]=right[j]
+                j+=1
+            k+=1
+
+        while i<len(left):
+            arr[k]=left[i]
+            i+=1
+            k+=1
+        while j<len(right):
+            arr[k]=right[j]
+            j+=1
+            k+=1
+          
+a = [44,53,5,2,3,11,4]
+merge(a)
+print(a)
