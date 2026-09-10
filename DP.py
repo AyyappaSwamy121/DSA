74239efc5165b7c0c6e19cc9d576a4568defc754
Ayyappa Swamy <153265218+AyyappaSwamy121@users.noreply.github.com>
2026-09-11 00:07:35 +0530

    Update DP.py

--- DP.py
+++ DP.py
@@ -12,13 +12,4 @@ n = 10
 print("Fibonacci using Memoization:", fib_memoization(n))
 
 
-def fib_tabulation(n):
-    dp = [0] * (n + 1)
-    dp[0] = 0
-    dp[1] = 1
-    for i in range(2, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2]
-    return dp[n]
 
-n = 10
-print("Fibonacci using Tabulation:", fib_tabulation(n))
